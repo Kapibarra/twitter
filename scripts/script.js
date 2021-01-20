723991dab04448696e5193e2dac47805968b281c
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-01-20 17:16:33 +0300

    q

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,38 @@
-
-
 class Posts {
-    constructor() {
+  constructor(param) {
+    this.tweets = param.posts;
+  }
+  addPost(tweet) {
+const post = new Post(tweet)
+this.tweets.push(post)
+  }
+  deletePost(id) {
 
-    }
+  }
+
+  likePost(id) {
+      
+  }
 }
-
 
 class Post {
-    constructor() {
-this.userName = param.userName
-this.nickname = param.nickname
-this.postDate = param.postDate
-this.text = param.text
-this.img = param.img
-this.likes = param.likes
-this.liked = false
+  constructor() {
+    this.id = param.id;
+    this.userName = param.userName;
+    this.nickname = param.nickname;
+    this.postDate = param.postDate;
+    this.text = param.text;
+    this.img = param.img;
+    this.likes = param.likes;
+    this.liked = false;
+  }
 
-}
-
-    changeLike() {
-this.liked = !this.liked
-if ( this.liked) {
-    this.likes++
-}
+  changeLike() {
+    this.liked = !this.liked;
+    if (this.liked) {
+      this.likes++;
+    } else {
+      this.likes--;
     }
+  }
 }
